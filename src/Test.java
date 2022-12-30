0b7b12ae3c1e291bd3d63b9b232539e7c573135a
PSYSHO <FIGURE0.9>
2022-12-30 18:46:18 +0400

    FirstTask

--- src/Test.java
+++ src/Test.java
@@ -1,2 +1,18 @@
+import model.Family;
+import model.FamilyRole;
+import model.Human;
+
 public class Test {
+   public static void main(String[] args){
+       Family myFamily = new Family();
+
+       myFamily.addHuman(new Human("Pit",FamilyRole.Father));
+       myFamily.addHuman(new Human("Alis",FamilyRole.Mather));
+       myFamily.addHuman(new Human("BOOY",FamilyRole.Child));
+       myFamily.addHuman(new Human("GIIIRLL",FamilyRole.Child));
+       myFamily.addHuman(new Human("Carl",FamilyRole.Child));
+
+       Human human = myFamily.findHuman("Pit");
+       System.out.println(human.getName() +"   "+human.getFamilyRole().toString()+ "\r\n" + myFamily.whatDo(human));
+   }
 }
